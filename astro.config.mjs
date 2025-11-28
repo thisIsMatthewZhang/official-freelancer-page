5031072fe4e3476ede8ef90b4afa006e3544fdeb
Matthew Zhang <zhangmatt22@gmail.com>
2025-11-28 13:40:04 -0600

    added deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte({ extensions: ['.svelte'] })],
+  site: 'https://thisIsMatthewZhang.github.io',
+  base: '/official-freelancer-page',
 
   vite: {
     plugins: [tailwindcss()]
